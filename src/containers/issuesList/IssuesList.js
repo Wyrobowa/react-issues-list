cd8a1261f1dd1ec167a0ecaad953bec58a1fb563
aniawyroba <aniawyroba@gmail.com>
2020-03-04 18:40:56 +0100

    added request service and fetch issues list data

--- src/containers/issuesList/IssuesList.js
+++ src/containers/issuesList/IssuesList.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Services
+import { fetchData } from '../../services/requestService';
 
 const IssuesList = () => {
+  const [issues, setIssues] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchData('http://localhost:3001/list');
+      setIssues(data);
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
       <p>List</p>
+      <div>
+        <div>Title</div>
+        <div>Description</div>
+        <div>State</div>
+      </div>
+      {issues && issues.length > 0 && issues.map((issue) => (
+        <div>
+          <div>{issue.title}</div>
+          <div>{issue.description}</div>
+          <div>{issue.state}</div>
+        </div>
+      ))}
     </>
   );
 };
